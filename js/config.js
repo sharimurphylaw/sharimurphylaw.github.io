0c9156f043336eee6b39a5b11c7d3591f7fec3cd
Anderson <s0044530@lstllandersonmac.rgare.net>
2019-04-24 17:57:57 -0500

    initial commit

---
+++ js/config.js
@@ -0,0 +1,40 @@
+// cpanel - site_templates/vcard_legal/assets/config.js.tt Copyright(c) 2016 cPanel, Inc.
+//                                                          All rights Reserved.
+// copyright@cpanel.net                                        http://cpanel.net
+// This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+window.cpanel = {
+    data: {
+        phone: "618-797-8888",
+        fax: "618-576-6239",
+        email: "staff@sharimurphylaw.com",
+        address: "P.O. Box 136\nWood River, IL  62095-0136",
+        social: [
+            
+            
+            
+            
+            
+            
+        ]
+    },
+    style: {
+        primary: "",
+        accent: ""
+    },
+    slides: [
+        {
+            prefix: "We are",
+            title: "The Law Offices of Shari L. Murphy, LLC",
+            subtitle: "",
+            type: 'vcard',
+            backgroundImage: "",
+            backgroundColor: "",
+            color: "",
+            buttonText: "",
+            buttonLink: "",
+            biography: "",
+            portraitImage: ""
+        }
+    ]
+};
